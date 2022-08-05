e9cafeedc4bebc745d37bc30e99363f33bc7ae70
HadafKurs <ilhamilhamhuseyneliyev@gmail.com>
2022-08-05 01:42:17 -0700

    print

--- hdf/src/components/navbarpage/testsubjectpage/testsubject4.jsx
+++ hdf/src/components/navbarpage/testsubjectpage/testsubject4.jsx
@@ -6,7 +6,7 @@ import "../../../CSS/website/branchpage.css";
 import { Link } from "react-router-dom";
 import { TabTitle } from "../../TitleFunction";
 import Table from 'react-bootstrap/Table';
-
+import PrintComponents from "react-print-components";
 const Testsubject4 = () => {
     TabTitle('Buraxılış fənləri üzrə MQT 5 mövzuları  Azərbaycan bölməsi');
   return (
@@ -15,6 +15,138 @@ const Testsubject4 = () => {
       <Navbar />
       <p className="text-center fw-bold branchheader">Buraxılış fənləri üzrə MQT 5 mövzuları  Azərbaycan bölməsi</p>
       <div className="branchbtn">
+      <div className="printbtncom">
+          <PrintComponents
+            trigger={
+              <button className="printbtn btn btn-dark">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-printer-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M5 1a2 2 0 0 0-2 2v1h10V3a2 2 0 0 0-2-2H5zm6 8H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1z" />
+                  <path d="M0 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-2a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2H2a2 2 0 0 1-2-2V7zm2.5 1a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" />
+                </svg>
+              </button>
+            }
+          >
+            <div className="container">
+              <Table striped="columns">
+                <thead>
+                  <th className="text-center fs-4">
+                    Buraxılış mövzuları 1 - 01/05/2022
+                  </th>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td className="text-center fw-bold">IX sinif </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <strong>Azərbaycan dili</strong> <br />
+                      Fonetika. Səs və hərf. Sait səslər. Saitlərin deyilişi və
+                      yazılışı. Ahəng qanunu. Samit səslər. Samitlərin deyilişi
+                      və yazılışı. Sözlərin sonunda cingiltili samitlərin
+                      yazılışı və tələffüzü. Sonu q və k ilə bitən sözlərin
+                      yazılışı və tələffüzü. . Qoşa samitlə bitən təkhecalı
+                      sözlər. Vurğu. Sözün sətirdən-sətrə keçirilməsi. Fonetik
+                      təhlil.
+                      <br />
+                      <strong>İngilis dili</strong>
+                      <br />
+                      Noun. To be. There is / are. It is. Vocabulary (V sinif
+                      unit 1– 8 daxil olmaqla). Listening and Reading
+                      Comprehension
+                      <br />
+                      <strong>Rus dili</strong>
+                      <br />
+                      Фонетика. Буквы ь и ъ. Двойная роль букв е, ё, ю, я.
+                      Лексикология. Лексическое значение слова. Прямое и
+                      переносное значение слова. Синонимы. Антонимы.
+                      <br />
+                      <strong>Riyaziyyat</strong>
+                      <br />
+                      Natural ədədlər. Adi və onluq kəsrlər.
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="text-center fw-bold">X sinif </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <strong>Azərbaycan dili</strong> <br />
+                      Fonetika. Səs və hərf. Sait səslər. Saitlərin deyilişi və
+                      yazılışı. Ahəng qanunu. Samit səslər. Samitlərin deyilişi
+                      və yazılışı. Sözlərin sonunda cingiltili samitlərin
+                      yazılışı və tələffüzü. Sonu q və k ilə bitən sözlərin
+                      yazılışı və tələffüzü. . Qoşa samitlə bitən təkhecalı
+                      sözlər. Vurğu. Sözün sətirdən-sətrə keçirilməsi. Fonetik
+                      təhlil. <br />
+                      <strong>İngilis dili</strong>
+                      <br />
+                      Noun. To be. There is / are. It is. Vocabulary (V sinif
+                      unit 1– 8 daxil olmaqla). Listening and Reading
+                      Comprehension
+                      <br />
+                      <strong>Rus dili</strong>
+                      <br />
+                      Фонетика. Буквы ь и ъ. Двойная роль букв е, ё, ю, я.
+                      Лексикология. Лексическое значение слова. Прямое и
+                      переносное значение слова. Синонимы. Антонимы.
+                      <br />
+                      <strong>Riyaziyyat</strong>
+                      <br />
+                      Natural ədədlər. Adi və onluq kəsrlər.
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="text-center fw-bold">XI sinif </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <strong>Azərbaycan dili</strong> <br />
+                      Fonetika. Danışıq səsləri. Ahəng qanunu. Bəzi saitlərin
+                      uzun tələffüz edilməsi. Qoşasaitli və qoşasamitli sözlərin
+                      yazılışı və tələffüzü. Əlifba. Sonu eynicinsli qoşa
+                      samitlə bitən söz köklərinin yazılışı. Sözlərin sonunda
+                      cingiltili samitlərin yazılışı və tələffüzü. Sonu q və k
+                      ilə bitən sözlərin yazılışı və tələffüzü. Heca. Vurğu.
+                      Sözün sətirdən sətrə keçirilməsi. Fonetik təhlil.
+                      <br />
+                      Leksika. Sözün leksik və qrammatik mənası. Sözün həqiqi və
+                      məcazi mənası. Təkmənalı və çoxmənalı sözlər. Omonimlər.
+                      Omonim və çoxmənalı sözlərin fərqi və oxşarlığı.
+                      Sinonimlər. Antonimlər. Ümumişlək və ümumişlək olmayan
+                      sözlər. Dialekt sözlər, terminlər (ixtisas sözlər),
+                      arxaizmlər, neologizmlər ümumişlək olmayan söz qrupları
+                      kimi. Sözlərin mənşəyi. Frazeoloji birləşmələr, onların
+                      omonimliyi, sinonimliyi, antonimliyi. Lüğətlər. Leksik
+                      təhlil. .<br />
+                      <strong>İngilis dili</strong>
+                      <br />
+                      Noun. To be. There is / are. It is. Vocabulary (V sinif
+                      unit 1– 8 daxil olmaqla). Listening and Reading
+                      Comprehension
+                      <br />
+                      <strong>Rus dili</strong>
+                      <br />
+                      Фонетика. Буквы ь и ъ. Двойная роль букв е, ё, ю, я.
+                      Лексикология. Лексическое значение слова. Прямое и
+                      переносное значение слова. Синонимы. Антонимы.
+                      <br />
+                      <strong>Riyaziyyat</strong>
+                      <br />
+                      Natural ədədlər. Adi və onluq kəsrlər.
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+            </div>
+          </PrintComponents>
+        </div>
       <Link to="/">
           <button type="button" className="branchbutton btn btn-warning w-20 ">
            Çıx
