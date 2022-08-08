6571899174f2eb39f1536ff20009f7643ab16319
HadafKurs <ilhamilhamhuseyneliyev@gmail.com>
2022-08-08 01:32:44 -0700

    hover

--- hdf/src/components/cFooter.jsx
+++ hdf/src/components/cFooter.jsx
@@ -16,13 +16,13 @@ function CFooter() {
             </a>
             
             <CDBBox display="flex" className="mt-4 Footertext ">
-            <a href="https://www.facebook.com/hedefkurslariofficial" target="_blank">  <CDBBtn flat color="info">
+            <a href="https://www.facebook.com/hedefkurslariofficial" target="_blank" >  <CDBBtn flat color="info">
                 <CDBIcon fab icon="facebook-f" />
               </CDBBtn></a>
-             <a href="https://twitter.com/intent/user?screen_name=hedefkurslari" target='_blank'> <CDBBtn flat color="info" className="mx-3">
+             <a href="https://twitter.com/intent/user?screen_name=hedefkurslari" target='_blank' > <CDBBtn flat color="info" className="mx-3">
                 <CDBIcon fab icon="twitter" />
               </CDBBtn></a>
-          <a href="https://www.instagram.com/accounts/login/?next=/hedef__kurslari/" target='_blank'><CDBBtn flat color="info" >
+          <a href="https://www.instagram.com/accounts/login/?next=/hedef__kurslari/" target='_blank' ><CDBBtn flat color="info" >
                 <CDBIcon fab icon="instagram" />
               </CDBBtn></a> 
             </CDBBox>
@@ -30,25 +30,25 @@ function CFooter() {
           <CDBBox>
            
             <CDBBox  flex="column" style={{ cursor: 'pointer', padding: '0' }}>
-             <Link to="/HadafQrup"> <CDBFooterLink className="footertext" href="#">Haqqında</CDBFooterLink></Link>
-            <Link to="/services"><CDBFooterLink className="footertext" href="#">Xidmətlər</CDBFooterLink></Link>
-            <Link to="/aplicantuseful"><CDBFooterLink className="footertext" href="#">Bloq</CDBFooterLink></Link>
+             <Link to="/HadafQrup"> <CDBFooterLink  href="#"><span className="footertext">Haqqında</span></CDBFooterLink></Link>
+            <Link to="/services"><CDBFooterLink className="footertext" href="#"><span className="footertext">Xidmətlər</span></CDBFooterLink></Link>
+            <Link to="/aplicantuseful"><CDBFooterLink className="footertext" href="#"><span className="footertext">Bloq</span></CDBFooterLink></Link>
               
             </CDBBox>
           </CDBBox>
           <CDBBox>
            
             <CDBBox className="Footertext" flex="column" style={{ cursor: 'pointer', padding: '0' }}>
-            <Link to="/news1"> <CDBFooterLink href="/">Xəbərlər</CDBFooterLink></Link>
-             <Link to="/testsubject1"> <CDBFooterLink href="/">Sınaq mövzuları</CDBFooterLink></Link>
+            <Link to="/news1"> <CDBFooterLink href="/"><span className="footertext">Xəbərlər</span></CDBFooterLink></Link>
+             <Link to="/testsubject1"> <CDBFooterLink href="/"><span className="footertext">Sınaq mövzuları</span></CDBFooterLink></Link>
               
             </CDBBox>
           </CDBBox>
           <CDBBox>
            
             <CDBBox className="Footertext" flex="column" style={{ cursor: 'pointer', padding: '0' }}>
-           <Link to="/faq"><CDBFooterLink href="#">FAQ</CDBFooterLink></Link>
-            <Link to="/ContactForm" ><CDBFooterLink href="#">Əlaqə</CDBFooterLink></Link>
+           <Link to="/faq"><CDBFooterLink href="#"><span className="footertext">FAQ</span></CDBFooterLink></Link>
+            <Link to="/ContactForm" ><CDBFooterLink href="#"><span className="footertext">Əlaqə</span></CDBFooterLink></Link>
               
             </CDBBox>
           </CDBBox>
